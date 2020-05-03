65222647f383f967515bcf16ec81929feb2e328f
Dylan Acup <dacup@ciscocems.com>
2020-05-02 20:08:08 -0600

    Main foundation created

--- apps/cylinder-tracker/src/app/app.module.ts
+++ apps/cylinder-tracker/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NxModule } from '@nrwl/angular';
+import { MaterialModule } from '@cedar-all/material';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule, 
+    NxModule.forRoot(),
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    MaterialModule,
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreModule.forRoot({}, {})
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

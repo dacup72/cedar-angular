fd84cce81c47fc5cdba2296d06ea3727387d9f68
Dylan Acup <dacup@ciscocems.com>
2021-02-26 10:18:05 -0700

    small updates to Tab letters for color and style

--- apps/cylinder-tracker/src/app/home/edit-cylinder-dialog/edit-cylinder-dialog.component.ts
+++ apps/cylinder-tracker/src/app/home/edit-cylinder-dialog/edit-cylinder-dialog.component.ts
@@ -20,101 +20,18 @@ export class EditCylinderDialogComponent {
   cylinderForm: FormGroup;
   origionalTitle: string;
   commonGasCodes = [ 'NO', 'NO2', 'NOX', 'CO', 'O2', 'SO2', 'CO2'];
-  // commonGasCodesOBJ = { 
-  //   NO: false,
-  //   NO2: false,
-  //   NOX: false,
-  //   CO: false,
-  //   O2: false,
-  //   SO2: false,
-  //   CO2: false
-  // };
-  uncommonGasCodes = ['N2O', 'PPN', 'CH4', 'HE', 'H2S']
-  // uncommonGasCodesOBJ = {
-  //   N2O: false,
-  //   PPN: false,
-  //   CH4: false,
-  //   HE: false,
-  //   H2S: false
-  // };
+  uncommonGasCodes = ['N2O', 'PPN', 'CH4', 'HE', 'H2S'];
   balCodes = ['BALN', 'BALA'];
-  // balCodesOBJ = {
-  //   BALN: false, 
-  //   BALA: false
-  // };
   otherCodes = ['APPVD', 'AIR', 'SRM', 'NTRM', 'GMIS', 'RGM', 'PRM', 'ZERO'];
-  // otherCodesOBJ = {
-  //   APPVD: false, 
-  //   AIR: false, 
-  //   SRM: false, 
-  //   NTRM: false, 
-  //   GMIS: false, 
-  //   RGM: false, 
-  //   PRM: false, 
-  //   ZERO: false
-  // };
-  measurementTypes = ['ppm', '%CO2', '%O2', 'ppb'];
-  // selectedGasCode = '';
+  measurementTypes = ['ppm', '%CO2', '%O2', 'ppb', '%'];
   showMoreGases = false;
-  //stringOfGasCodes = '';
-
-  // gasColors = {
-  //   $cylindertrackerso2: '#9DCC5F',
-  //   $cylindertrackerso2text: 'black',
-  //   $cylindertrackerso2secondary: '#8E5FCC',
-  //   $cylindertrackerso2secondarytext: 'white',
-  //   $cylindertrackerno: '#36D6E7',
-  //   $cylindertrackernotext: 'black',
-  //   $cylindertrackernosecondary: '#E74736',
-  //   $cylindertrackernosecondarytext: 'black',
-  //   $cylindertrackerno2: '#9BCCFD',
-  //   $cylindertrackerno2text: 'black',
-  //   $cylindertrackerno2secondary: '#FDCC9B',
-  //   $cylindertrackerno2secondarytext: 'black',
-  //   $cylindertrackernox: '#0074DD',
-  //   $cylindertrackernoxtext: 'white',
-  //   $cylindertrackernoxsecondary: '#DD6900',
-  //   $cylindertrackernoxsecondarytext: 'black',
-  //   $cylindertrackern2o: '#81599F',
-  //   $cylindertrackern2otext: 'white',
-  //   $cylindertrackern2osecondary: '#779F59',
-  //   $cylindertrackern2osecondarytext: 'black',
-  //   $cylindertrackerco2: '#FD7F20',
-  //   $cylindertrackerco2text: 'black',
-  //   $cylindertrackerco2secondary: '#209EFD',
-  //   $cylindertrackerco2secondarytext: 'black',
-  //   $cylindertrackerco: '#FDB750',
-  //   $cylindertrackercotext: 'black',
-  //   $cylindertrackercosecondary: '#5096FD',
-  //   $cylindertrackercosecondarytext: 'black',
-  //   $cylindertrackero2: '#335120',
-  //   $cylindertrackero2text: 'white',
-  //   $cylindertrackero2secondary: '#3E2051',
-  //   $cylindertrackero2secondarytext: 'white',
-  //   $cylindertrackerppn: '#660A60',
-  //   $cylindertrackerppntext: 'white',
-  //   $cylindertrackerppnsecondary: '#0A6610',
-  //   $cylindertrackerppnsecondarytext: 'white',
-  //   $cylindertrackerch4: '#FF5C4D',
-  //   $cylindertrackerch4text: 'black',
-  //   $cylindertrackerch4secondary: '#4DF0FF',
-  //   $cylindertrackerch4secondarytext: 'black',
-  //   $cylindertrackerhe: '#FFCD58',
-  //   $cylindertrackerhetext: 'black',
-  //   $cylindertrackerhesecondary: '#588AFF',
-  //   $cylindertrackerhesecondarytext: 'black',
-  //   $cylindertrackerh2s: '#FF5C4D',
-  //   $cylindertrackerh2stext: 'black',
-  //   $cylindertrackerh2ssecondary: '#4DF0FF',
-  //   $cylindertrackerh2ssecondarytext: 'black',
-  // }
-    // TODO find better way to make dynamic colors for gases
+  showMoreOtherGases = false;
+  balCodeSelected = false;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private dialogRef: MatDialogRef<EditCylinderDialogComponent>, fb: FormBuilder) {
     this.currentCylinder = !data.cylinder ? cloneDeep(emptyCylinder) : cloneDeep(data.cylinder);
     this.origionalTitle = !data.cylinder ? '' : data.cylinder.cylinderID;
     
-    // Build new form control for cylinder
     this.cylinderForm = fb.group({
       cylinderID: [this.currentCylinder.cylinderID, 
         Validators.required,
@@ -139,6 +56,13 @@ export class EditCylinderDialogComponent {
 
     this.currentCylinder.epaGasTypeCodes.forEach(c => this.addEPAGasTypeCode(c));
     this.currentCylinder.componentGases.forEach(g => this.addComponentGas(g.qaGasDefCode, g.epaGasCode, g.gasConcentration, g.uom));
+
+    // Add BALN as default if adding new cylinder
+    if (!this.currentCylinder.id) {
+      this.addEPAGasTypeCode('BALN');
+    }
+    this.isBalCodeSelected();
+
   }
 
   get cylinderID() {
@@ -173,20 +97,22 @@ export class EditCylinderDialogComponent {
   }
 
   addComponentGas(qaGasDefCode: string, epaGasCode = qaGasDefCode, gasConc = 0, uom = '') {
-    this.componentGases.push(new FormGroup({
-      qaGasDefCode: new FormControl(qaGasDefCode,
-        Validators.required,
-      ),
-      epaGasCode: new FormControl(epaGasCode,
-        Validators.required,
-      ),
-      gasConcentration: new FormControl(gasConc,
-        Validators.required,
-      ),
-      uom: new FormControl(uom,
-        Validators.required,
-      )
-    }))
+    if(!this.balCodes.includes(qaGasDefCode) && !this.otherCodes.includes(qaGasDefCode)) {
+      this.componentGases.push(new FormGroup({
+        qaGasDefCode: new FormControl(qaGasDefCode,
+          Validators.required,
+        ),
+        epaGasCode: new FormControl(epaGasCode,
+          Validators.required,
+        ),
+        gasConcentration: new FormControl(gasConc,
+          Validators.required,
+        ),
+        uom: new FormControl(uom,
+          Validators.required,
+        )
+      }))
+    }
   }
 
   removeComponentGases(gasCodes: string[]) {
@@ -197,12 +123,18 @@ export class EditCylinderDialogComponent {
     });
   }
 
-  newGasCodeChange(componentGas: HTMLInputElement) {
+  gasCodeChange(componentGas: HTMLInputElement) {
     switch (componentGas.name) {
       case 'gasCode':
         if (componentGas.checked) {
           this.addComponentGas(componentGas.value);
           this.addEPAGasTypeCode(componentGas.value);
+          this.clearOtherCodes();
+          
+          // BALA and O2 are mutually exclusive
+          if (componentGas.value === 'O2') {
+            this.removeEPAGasTypeCodes(['BALA']);
+          }
         }
         else {
           this.removeComponentGases([componentGas.value]);
@@ -212,11 +144,17 @@ export class EditCylinderDialogComponent {
 
       case 'balCode':
         if (componentGas.checked) {
-          this.newClearBalCodesExceptSelf(componentGas.value);
-          this.newClearOtherCodes();
+          this.clearBalCodesExceptSelf(componentGas.value);
+          this.clearOtherCodes();
 
           this.addComponentGas(componentGas.value);
           this.addEPAGasTypeCode(componentGas.value);
+
+          // BALA and O2 are mutually exclusive
+          if (componentGas.value === 'BALA') {
+            this.removeEPAGasTypeCodes(['O2']);
+            this.removeComponentGases(['O2']);
+          }
           
         }
         else {
@@ -227,18 +165,25 @@ export class EditCylinderDialogComponent {
       
       case 'otherCode':
         if (componentGas.checked) {
-          this.newClearOtherCodesButSelf(componentGas.value);
-          this.newClearBalCodes();
-          this.newClearCommonCodes();
+          this.clearOtherCodesExceptSelf(componentGas.value);
+          this.clearBalCodes();
+          this.clearCommonCodes();
 
           if (componentGas.value === 'AIR') {
             this.addComponentGas('O2', 'O2', 20.9, '%O2');
             this.addEPAGasTypeCode('O2');
+            this.addEPAGasTypeCode('AIR');
             this.cylinderID.setValue('Inst Air');
+
+            // Update to create real date time / fix to auto select from date picker
+            this.expirationDate.setValue('1/1/2200');
           }
           else {
             this.addComponentGas(componentGas.value);
             this.addEPAGasTypeCode(componentGas.value);
+
+            // This is required since AIR has different value "O2"
+            this.removeEPAGasTypeCodes(['AIR']);
           }
         }
         else {
@@ -246,6 +191,9 @@ export class EditCylinderDialogComponent {
             this.removeComponentGases(['O2']);
             this.removeEPAGasTypeCodes(['O2']);
             this.cylinderID.setValue(this.origionalTitle);
+
+            // Needs to deselect from date picker
+            this.expirationDate.setValue('');
           }
           else {
             this.removeComponentGases([componentGas.value]);
@@ -261,38 +209,39 @@ export class EditCylinderDialogComponent {
       default:
         break;
     }
+    this.isBalCodeSelected();
   }
 
-  newClearOtherCodes() {
+  clearOtherCodes() {
     this.removeComponentGases(this.otherCodes);
     this.removeEPAGasTypeCodes(this.otherCodes);
   }
 
-  newClearOtherCodesButSelf(code) {
+  clearOtherCodesExceptSelf(code) {
     const filteredOtherCodes = this.otherCodes.filter(c => c !== code);
     this.removeComponentGases(filteredOtherCodes);
     this.removeEPAGasTypeCodes(filteredOtherCodes);
   }
 
-  newClearBalCodes() {
+  clearBalCodes() {
     this.removeComponentGases(this.balCodes);
     this.removeEPAGasTypeCodes(this.balCodes);
   }
 
-  newClearBalCodesExceptSelf(code) {
+  clearBalCodesExceptSelf(code) {
    const filteredBalCodes = this.balCodes.filter(c => c !== code);
    this.removeComponentGases(filteredBalCodes);
    this.removeEPAGasTypeCodes(filteredBalCodes);
   }
 
-  newClearCommonCodes() {
+  clearCommonCodes() {
     this.removeComponentGases(this.commonGasCodes);
     this.removeComponentGases(this.uncommonGasCodes);
     this.removeEPAGasTypeCodes(this.commonGasCodes);
     this.removeEPAGasTypeCodes(this.uncommonGasCodes);
   }
 
-  newClearCylinderInputs() {
+  clearCylinderInputs() {
     this.cylinderID.setValue('');
     this.vendorID.setValue('');
     this.expirationDate.setValue('');
@@ -316,207 +265,40 @@ export class EditCylinderDialogComponent {
     return gasCodeString;
   }
 
-  newOnClearCylinder() {
-    this.newClearBalCodes();
-    this.newClearCommonCodes();
-    this.newClearOtherCodes();
-    this.newClearCylinderInputs();
+  isBalCodeSelected() {
+    let commonOrUncommonGasSelected = false;
+    this.balCodeSelected = true;
+
+    // BALN or BALA is required only if a common / uncommon gas is selected
+    this.epaGasTypeCodes.value.forEach(code => {
+      if (this.commonGasCodes.includes(code) || this.uncommonGasCodes.includes(code)) {
+        commonOrUncommonGasSelected = true;
+      }
+    })
+
+    // Override if 'AIR' exists
+    if (this.epaGasTypeCodes.value.includes('AIR')) {
+      commonOrUncommonGasSelected = false;
+    }
+
+    if (commonOrUncommonGasSelected) {
+      this.balCodeSelected = false;
+      this.epaGasTypeCodes.value.forEach(code => {
+        if (this.balCodes.includes(code)) {
+          this.balCodeSelected = true;
+        }
+      });
+    }
+  }
+
+  onClearCylinder() {
+    this.clearBalCodes();
+    this.clearCommonCodes();
+    this.clearOtherCodes();
+    this.clearCylinderInputs();
   }
 
   onCancel() {
     this.dialogRef.close();    
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // ngAfterViewInit(): void {
-  //   // TODO: this produces a console error message, fix it
-  //   this.currentCylinder.epaGasTypeCodes.forEach(c => {
-  //     this.commonGasCodesOBJ[c] = true;
-  //     this.balCodesOBJ[c] = true;
-
-  //     if(!this.stringOfGasCodes.length) {
-  //       this.stringOfGasCodes = c.toUpperCase();
-  //     }
-  //     else if(!this.stringOfGasCodes.split(', ').includes(c.toUpperCase())) {
-  //       let newArr = this.stringOfGasCodes.split(', ');
-  //       newArr.push(c.toUpperCase());
-  //       this.stringOfGasCodes = newArr.join(', ');
-  //     }
-  //   })
-  // }
-
-  // cylIDFormControl = new FormControl('', [
-  //   Validators.required
-  // ]);
-
-  // expDateFormControl = new FormControl('', [
-  //   Validators.required
-  // ]);
-
-  // vendorIDFormControl = new FormControl('', [
-  //   Validators.required
-  // ]);
-
-  // gasCodeStringFormControl = new FormControl('', [
-  //   Validators.required
-  // ]);
-
-  // gasCodeChange(event, gasCode) {
-  //   // update current cylinder
-  //   if(event.checked) {
-  //     this.clearOtherCodes();
-  //     this.currentCylinder.epaGasTypeCodes = this.currentCylinder.epaGasTypeCodes.concat([gasCode]);
-  //     const emptyComponentGas: ComponentGas = {
-  //       qaGasDefCode: gasCode,
-  //       epaGasCode: gasCode,
-  //       gasConcentration: null,
-  //       uom: null
-  //     }
-  //     this.currentCylinder.componentGases = this.currentCylinder.componentGases.concat([emptyComponentGas]);
-  //   }
-  //   else {
-  //     this.currentCylinder.epaGasTypeCodes = this.currentCylinder.epaGasTypeCodes.filter(code => code !== gasCode);
-  //     this.currentCylinder.componentGases = this.currentCylinder.componentGases.filter(gas => gas.epaGasCode !== gasCode);
-  //   }
-
-  //   // Update gas code string
-  //   if(!event.checked && this.stringOfGasCodes.split(', ').includes(gasCode.toUpperCase())) {
-  //     let newArr = this.stringOfGasCodes.split(', ');
-  //     newArr.splice(newArr.indexOf(gasCode.toUpperCase()), 1);
-  //     this.stringOfGasCodes = newArr.join(', ');
-  //   }
-  //   else if(!this.stringOfGasCodes.length) {
-  //     this.stringOfGasCodes = gasCode.toUpperCase();
-  //   }
-  //   else if(!this.stringOfGasCodes.split(', ').includes(gasCode.toUpperCase())) {
-  //     let newArr = this.stringOfGasCodes.split(', ');
-  //     newArr.push(gasCode.toUpperCase());
-  //     this.stringOfGasCodes = newArr.join(', ');
-  //   }
-  //   //console.log(this.commonGasCodesCodesOBJ)
-    
-  // }
-
-  // gasTypeSelected(event) {
-  //   //console.log(event)
-  // }
-
-  // getMeasurementValue(componentGas) {
-  //   const selectedGas = this.currentCylinder.componentGases.filter(gas => gas.epaGasCode === componentGas);
-  //   return selectedGas.length > 0  ? selectedGas[0].gasConcentration : '';
-  // }
-
-  // getMeasurementType(componentGas) {
-  //   const selectedGas = this.currentCylinder.componentGases.filter(gas => gas.epaGasCode === componentGas);
-  //   return selectedGas.length > 0 ? selectedGas[0].uom : '';
-  // }
-
-  // measurementValueChange(event, componentGas) {
-  //   this.currentCylinder.componentGases.filter(gas => gas.epaGasCode === componentGas)[0].gasConcentration = event.target.value;
-  // }
-
-  // measurementTypeChange(event, componentGas) {
-  //   this.currentCylinder.componentGases.filter(gas => gas.epaGasCode === componentGas)[0].uom = event.value;
-  // }
-
-  // resetCurrentCylinder() {
-    
-  //   this.currentCylinder = cloneDeep(emptyCylinder);
-    
-  // }
-
-  // onClearCylinder() {
-  //   this.resetCurrentCylinder();
-  //   this.clearCommonCodes();
-  //   this.clearBalCodes();
-  //   this.clearOtherCodes();
-  //   this.stringOfGasCodes = '';
-  // }
-
-  // gasCodeStringChange(event) {
-  //   console.log(event.target.value)
-  // }
-
-  // balCodeChange(event, code) {
-  //   if(event.checked) {
-  //     this.clearBalCodesExceptSelf(code);
-  //     this.clearOtherCodes();
-  //   }
-  // }
-
-  // otherCodeChanged(event, code) {
-  //   if(event.checked) {
-  //     this.clearOtherCodesButSelf(code);
-  //     this.clearBalCodes();
-  //     this.clearCommonCodes();
-  //     this.currentCylinder.epaGasTypeCodes = [];
-  //     this.currentCylinder.componentGases = [];
-  //     this.stringOfGasCodes = '';
-
-  //     if(code === 'AIR') {
-  //       this.currentCylinder.componentGases = [
-  //         {
-  //           qaGasDefCode: 'O2',
-  //           epaGasCode: 'O2',
-  //           gasConcentration: 20.9,
-  //           uom: '%O2'
-  //         }
-  //       ];
-  //       this.currentCylinder.epaGasTypeCodes = ['O2'];
-  //       this.currentCylinder.cylinderID = 'Inst Air';
-  //       this.stringOfGasCodes = 'O2';
-  //       this.commonGasCodesOBJ['O2'] = true;
-  //     }
-  //   }
-  // }
-
-  // clearCommonCodes() {
-  //   this.commonGasCodes.forEach(gas => {
-  //     this.commonGasCodesOBJ[gas] = false;
-  //   })
-  // }
-
-  // clearBalCodesExceptSelf(balCode) {
-  //   this.balCodes.forEach(code => {
-  //     if(code !== balCode) {
-  //       this.balCodesOBJ[code] = false;
-  //     }
-  //   })
-  // }
-
-  // clearBalCodes() {
-  //   this.balCodes.forEach(code => {
-  //     this.balCodesOBJ[code] = false;
-  //   })
-  // }
-
-  // clearOtherCodesButSelf(otherCode) {
-  //   this.otherCodes.forEach(code => {
-  //     if(code !== otherCode) {
-  //       this.otherCodesOBJ[code] = false;
-  //     }
-  //   })
-  // }
-
-  // clearOtherCodes() {
-  //   this.otherCodes.forEach(code => {
-  //     this.otherCodesOBJ[code] = false;
-  //   })
-  // }
-
 }
